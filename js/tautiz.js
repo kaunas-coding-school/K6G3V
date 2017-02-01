db39a0726a6e3849dff401284274c2c67d8b52a1
Tautvydas <tautvydas@dulskis.eu>
2017-02-01 21:44:14 +0200

    Sukurtas naujas funkcionalumas paspaudus ant profilio nuotraukos ja sumaziname arba padidiname.

--- js/tautiz.js
+++ js/tautiz.js
@@ -1,3 +1,26 @@
-/**
- * Created by LenovoUser on 2/1/2017.
- */
+$(document).ready(function () {
+    $(".btn").click(function () {
+        var spalva = getRandomColor();
+        $(this).css('color',spalva);
+        console.debug('Spalva', spalva);
+        // alert('Pakeiciau spalva i: ' + spalva);
+    });
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    $('.card').on('click',function(){
+        $(this).find('img').animate({
+            height: 'toggle',
+            opacity: 'toggle'
+        }, 500)
+
+    });
+
+});
