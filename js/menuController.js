b71f9a1c6e718c4d6a2d160770fc7a694d2afe17
KamiZut <kzutautaite@gmail.com>
2017-02-03 21:25:34 +0200

    Merge branches 'develop', 'developl', 'jonas' and 'naujaUzduotis' of https://github.com/kaunas-coding-school/K6G3V into naujaUzduotis
    # Conflicts:
    #	kolegos/JonasP.html

--- js/menuController.js
+++ js/menuController.js
@@ -4,6 +4,7 @@ app.controller('menuController', function(){
     vm.menu = [
         {'url':'/K6G3V/', 'name':'Padrindinis'},
         {'url':'/K6G3V/kolegos/tautiz.html', 'name':'Tautvydas'},
-        {'url':'/K6G3V/kolegos/raimondas.html', 'name':'Raimondas'}
+        {'url':'/K6G3V/kolegos/raimondas.html', 'name':'Raimondas'},
+        {'url':'/K6G3V/kolegos/Kami.html', 'name':'Kami'}
     ];
 });
