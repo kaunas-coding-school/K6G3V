a06acb4a4eedc9e3935e42418d6171db61149c0f
raimon2kas <rrimce221@yahoo.com>
2017-02-03 21:29:28 +0200

    Raimondas menu

--- js/menuController.js
+++ js/menuController.js
@@ -2,7 +2,7 @@ app.controller('menuController', function(){
     var vm = this;
 
     vm.menu = [
-        {'url':'/K6G3V/', 'name':'Padrindinis'},
+        {'url':'/K6G3V/','name':'Padrindinis'},
         {'url':'/K6G3V/kolegos/tautiz.html', 'name':'Tautvydas'},
         {'url':'/K6G3V/kolegos/raimondas.html', 'name':'Raimondas'},
         {'url':'/K6G3V/kolegos/Kami.html', 'name':'Kami'}
