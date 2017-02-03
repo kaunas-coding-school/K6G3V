8cc02542f3bded048b37881a7056632eb121d987
jonmenas <ptakauskas.jonas@gmail.com>
2017-02-03 21:22:22 +0200

    atnaujintas JonasP failas

--- js/tautvydoControllers/tautvydoController.js
+++ js/tautvydoControllers/tautvydoController.js
@@ -13,5 +13,10 @@ app.controller('testCtrl', function($scope){
     $scope.keisti = function() {
         $scope.vardas = 'Kitas vardas ';
     }
+    $scope.maasyvas={'pavadinimas':'Pav pavadinimas','adresas':'https://scontent.fvno1-1.fna.fbcdn.net/v/t1.0-1/c27.0.160.160/p160x160/16472826_1124575140998654_8410973630367901586_n.jpg?oh=582480a7a40a1b2bc3c96e9674446dc4&oe=593FA565'},
+        {'pavadinimas':'Pav pavadinimas1','adresas':'https://scontent.fvno1-1.fna.fbcdn.net/v/t1.0-1/c27.0.160.160/p160x160/16472826_1124575140998654_8410973630367901586_n.jpg?oh=582480a7a40a1b2bc3c96e9674446dc4&oe=593FA565'},
+        {'pavadinimas':'Pav pavadinimas2','adresas':'https://scontent.fvno1-1.fna.fbcdn.net/v/t1.0-1/c27.0.160.160/p160x160/16472826_1124575140998654_8410973630367901586_n.jpg?oh=582480a7a40a1b2bc3c96e9674446dc4&oe=593FA565'},
+        {'pavadinimas':'Pav pavadinimas3','adresas':'https://scontent.fvno1-1.fna.fbcdn.net/v/t1.0-1/c27.0.160.160/p160x160/16472826_1124575140998654_8410973630367901586_n.jpg?oh=582480a7a40a1b2bc3c96e9674446dc4&oe=593FA565'};
+
 
 });
