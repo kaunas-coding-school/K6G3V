d1310c205a3cc4ebf98049d8a5ed9cb31c7523f4
laurynasg <laurynas.g@gmail.com>
2017-02-03 21:28:12 +0200

    Sasdfg

--- js/menuController.js
+++ js/menuController.js
@@ -3,6 +3,7 @@ app.controller('menuController', function(){
 
     vm.menu = [
         {'url':'/K6G3V/', 'name':'Padrindinis'},
-        {'url':'kolegos/tautiz.html', 'name':'Tautvydas'}
+        {'url':'/K6G3V/kolegos/tautiz.html', 'name':'Tautvydas'},
+        {'url':'/K6G3V/kolegos/Laurynas%20Gauronskis.html', 'name':'Laurynas'}
     ];
 });
