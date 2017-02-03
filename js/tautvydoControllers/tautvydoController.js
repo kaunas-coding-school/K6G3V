3081fcc08fd8c4b9fa715def8e2baeb80c25aad9
Tautvydas <tautvydas@dulskis.eu>
2017-02-03 21:15:30 +0200

    Menu kontrolleris.

--- js/tautvydoControllers/tautvydoController.js
+++ js/tautvydoControllers/tautvydoController.js
@@ -12,6 +12,15 @@ app.controller('testCtrl', function($scope){
 
     $scope.keisti = function() {
         $scope.vardas = 'Kitas vardas ';
-    }
+    };
+
+    $scope.masyvas = [
+        {'pavadinimas':'Pav pavadinimas 1', 'adresas':'http://domernas.lt/img/paveikslelis1.jpg'},
+        {'pavadinimas':'Pav pavadinimas 2', 'adresas':'http://domernas.lt/img/paveikslelis2.jpg'},
+        {'pavadinimas':'Pav pavadinimas 3', 'adresas':'http://domernas.lt/img/paveikslelis3.jpg'},
+        {'pavadinimas':'Pav pavadinimas 4', 'adresas':'http://domernas.lt/img/paveikslelis4.jpg'},
+        {'pavadinimas':'Pav pavadinimas 5', 'adresas':'http://domernas.lt/img/paveikslelis5.jpg'},
+        {'pavadinimas':'Pav pavadinimas 5', 'adresas':'http://domernas.lt/img/paveikslelis5.jpg'}
+    ];
 
 });
