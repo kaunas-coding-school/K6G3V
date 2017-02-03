a3481ab5316232f9a6d21ab1e50abc7bd4e1de49
laurynasg <laurynas.g@gmail.com>
2017-02-03 21:03:23 +0200

    Merge branch 'developl' into develop
    # Conflicts:
    #	js/daumanto.js
    #	js/raimondas.js
    #	kolegos/Daumantas.html
    #	kolegos/raimondas.html

--- js/lauControllers/lauController.js
+++ js/lauControllers/lauController.js
@@ -1,5 +1,3 @@
-var app = angular.module('myApp', []);
-
 app.controller ('myCtrl', function ($scope) {
     $scope.vardas = "Vardas";
     $scope.zinute = "mano zinute";
@@ -7,6 +5,31 @@ app.controller ('myCtrl', function ($scope) {
     $scope.keisti = function () {
         $scope.vardas = 'Send';
 
-    }
+    };
+
+    $scope.masyvas = [
+        {'pavadinimas':'Pav pavadinimas 1', 'nuoroda':'adreso nuoroda1'},
+        {'pavadinimas':'Pav pavadinimas 2', 'nuoroda':'adreso nuoroda1'},
+        {'pavadinimas':'Pav pavadinimas 3', 'nuoroda':'adreso nuoroda1'},
+        {'pavadinimas':'Pav pavadinimas 4', 'nuoroda':'adreso nuoroda1'},
+        {'pavadinimas':'Pav pavadinimas 5', 'nuoroda':'adreso nuoroda1'}
+    ];
+
+    $scope.menuMasyvas = [
+        {'adresas':'/K6G3V/index.html','name':'Pirmas puslapis'},
+        {'adresas':'/K6G3V/antras_puslapis.html','name':'Antras puslapis'},
+        {'adresas':'/K6G3V/trecias_puslapis.html','name':'Trecias puslapis'},
+        {'adresas':'Daumantas.html','name':'Daumantas'},
+        {'adresas':'JonasP.html','name':'Jonas'},
+        {'adresas':'Kami.html','name':'Kami'},
+        {'adresas':'klarkas.html','name':'Klarkas'},
+        {'adresas':'Laurynas%20Gauronskis.html','name':'Laurynas'},
+        {'adresas':'raimondas.html','name':'Raimondas'},
+        {'adresas':'tautiz.html','name':'Tautiz'},
+        {'adresas':'Ignas.html','name':'Ignas'}
+
+    ]
+
+
 
 });
