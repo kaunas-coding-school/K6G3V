8a101be489a0b4b2912216ff4b12dc22b041bda0
Ignas <krampasignas@gmail.com>
2017-02-04 20:38:19 +0200

    naujas

--- js/mainApp.js
+++ js/mainApp.js
@@ -8,9 +8,6 @@ app.config(function($routeProvider) {
         .when("/tautvydas", {
             templateUrl : "/K6G3V/kolegos/tautiz.html"
         })
-        .when("/ignas", {
-            templateUrl : "/K6G3V/kolegos/Ignas.html"
-        })
         .when("/Laurynas_Gauronskis", {
             templateUrl : "/K6G3V/kolegos/Laurynas%20Gauronskis.html"
         })
