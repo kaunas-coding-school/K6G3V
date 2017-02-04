6c66d6309dde2fdb24fd738a7e5747ac9d5879a7
KamiZut <kzutautaite@gmail.com>
2017-02-04 20:34:41 +0200

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	js/mainApp.js
    #	js/menuController.js

--- js/mainApp.js
+++ js/mainApp.js
@@ -12,7 +12,8 @@ app.config(function($routeProvider) {
             templateUrl : "/K6G3V/kolegos/Laurynas%20Gauronskis.html"
         })
         .when("/raimondas", {
-            templateUrl : "/K6G3V/kolegos/raimondas.html"
+            templateUrl: "/K6G3V/kolegos/raimondas.html"
+        })
         .when("/kami", {
             templateUrl : "/K6G3V/kolegos/Kami.html"
         })
