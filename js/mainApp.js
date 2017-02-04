ee56f1f346f7d435602bc64b4c3c9e31551a18ac
Tautvydas <tautvydas@dulskis.eu>
2017-02-04 20:26:05 +0200

    routing

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,1 +1,11 @@
 var app = angular.module('myApp', ['ngAnimate','ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "/K6G3V/index.htm"
+        })
+        .when("/tautvydas", {
+            templateUrl : "/K6G3V/kolegos/tautiz.html"
+        })
+});
